99a56f7164bfbc0786b18dd39e67397f5ed2ec88
Tomas Baca <Tomas Baca>
2026-06-19 21:17:17 +0200

    swapped _mt executors for _events_cbg

--- launch/flightforge_simulator.launch.py
+++ launch/flightforge_simulator.launch.py
@@ -92,7 +92,7 @@ def generate_launch_description():
         namespace='',
         name=namespace+'_container',
         package='rclcpp_components',
-        executable='component_container_mt',
+        executable='component_container_events_cbg',
         output="screen",
 
         # prefix=['debug_roslaunch ' + os.ttyname(sys.stdout.fileno())],
--- launch/hw_api.launch.py
+++ launch/hw_api.launch.py
@@ -86,7 +86,7 @@ def generate_launch_description():
         namespace=uav_name,
         name=namespace+'_container',
         package='rclcpp_components',
-        executable='component_container_mt',
+        executable='component_container_events_cbg',
         output="screen",
 
         # prefix=['debug_roslaunch ' + os.ttyname(sys.stdout.fileno())],
